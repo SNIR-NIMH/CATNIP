ba9f98048c934ebd5ce9e85f23fe34e9545dcd22
snehashis-roy <snehashis.roy@gmail.com>
2023-01-01 15:56:19 -0500

    Potential memory improvement

--- ApplyFRST.sh
+++ ApplyFRST.sh
@@ -3,22 +3,25 @@
 exe_name=$0
 exe_dir=`dirname "$0"`
 
-if [ $# -lt "3" ]; then
+if [ $# -lt "4" ]; then
   echo "
   Usage:
-  ./ApplyFRST.sh INDIR OUTDIR NUMCPU  RADII [Downsampled_Image]
+  ./ApplyFRST.sh INDIR OUTDIR NUMCPU  RADII [Downsampled_Image] [Gradient_Threshold]
 
     INDIR           Input directory with channel 640 tif image, or a 3D volume
     OUTDIR          Output directory where FRST transform files are written,
                     or a single tif file, which will be written as 3D tif
                     stack, e.g. FRST.tif
-    NUMCPU          Number of parallel processes to be used
+    NUMCPU          Number of parallel cpus to be used
     RADII           Vector of radii at which to compute transform. Comma
                     separated string if used in compiled code. Default is 2,3,4
     Downsampled_Img (Optional) A downsampled background-removed image to compute
                     scaling factor. If it is not mentioned, then the original
                     image (i.e. input_dir) will be downsampled by 6x6x5. For very
-                    large images, use the 640_downsampled_AxBxC_brain.nii.gz
+                    large images, use the _brain.nii.gz 
+    Gradient_Thresh (Optional) Gradient threshold for FRST. Default 0.5. A higher
+                    threshold is used to remove more noise. It is a fraction
+                    between 0 and 1.
   "
   exit 1
 fi
